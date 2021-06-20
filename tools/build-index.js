aab2d498b0a0ed4426e1859c9e0011f0436b2fb7
Adalbero F. Guimaraes <a.fernandesguimaraes@fme.de>
2021-06-20 19:02:15 +0200

    build-index

--- tools/build-index.js
+++ tools/build-index.js
@@ -1,1 +1,102 @@
-console.log('build index');
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+main();
+function main() {
+    console.log('BEGIN');
+    var ROOT = '../src/articles';
+    var articles = [];
+    indexArticles(articles, ROOT);
+    writeIndex(articles, ROOT + "/_index.json");
+    console.log('Articles:', articles);
+    console.log('END');
+}
+function writeIndex(articles, path) {
+    if (articles.length == 0) {
+        throw new Error('Not articles to write.');
+    }
+    var json = {
+        updated: new Date().toISOString(),
+        articles: articles
+    };
+    var data = JSON.stringify(json, null, 2);
+    fs.writeFile(path, data, function (err) {
+        if (err)
+            throw err;
+    });
+}
+function indexArticles(articles, root, path, file) {
+    var newPath = join([path, file], '/');
+    var fullPath = join([root, path, file], '/');
+    console.log('newPath', newPath);
+    if (isDir(fullPath)) {
+        var files = fs.readdirSync(fullPath);
+        files.forEach(function (file) { return indexArticles(articles, root, newPath, file); });
+    }
+    else if (file === null || file === void 0 ? void 0 : file.match(/\.md$/)) {
+        var articlePath_1 = (path === null || path === void 0 ? void 0 : path.substring(1)) || '';
+        var tags_1 = [];
+        var title_1 = '';
+        var values_1;
+        var NAME_REGEXP = /(.+)\.md$/g;
+        values_1 = extractText(NAME_REGEXP, file);
+        var name_1 = (values_1 ? values_1[0] : file).toLowerCase();
+        var id = articlePath_1 + "/" + name_1;
+        var lines = readFile(fullPath);
+        lines.forEach(function (line) {
+            var TITLE_REGEXP = /^\s*#\s+(.+)/g;
+            if (!title_1) {
+                values_1 = extractText(TITLE_REGEXP, line);
+                if (values_1)
+                    title_1 = values_1[0];
+            }
+            var TAG_REGEXP = /#([a-z.]+)/g;
+            values_1 = extractText(TAG_REGEXP, line);
+            if (values_1) {
+                pushTags(tags_1, values_1);
+            }
+            pushTags(tags_1, (articlePath_1 === null || articlePath_1 === void 0 ? void 0 : articlePath_1.split('/')) || []);
+        });
+        title_1 = title_1 || name_1;
+        var article = {
+            id: id,
+            title: title_1,
+            path: articlePath_1,
+            file: file,
+            tags: tags_1
+        };
+        articles.push(article);
+    }
+}
+function join(vet, sep) {
+    return vet.reduce(function (acc, val) { return (acc || '') + (val ? sep + val : ''); });
+}
+function pushTags(tags, values) {
+    values.forEach(function (value) {
+        if (!tags.includes(value)) {
+            tags.push(value);
+        }
+    });
+}
+function extractText(regexp, str) {
+    var values = [];
+    var group;
+    while ((group = regexp.exec(str))) {
+        values.push(group[1]);
+    }
+    return values.length > 0 ? values : undefined;
+}
+function readFile(path) {
+    var buff = fs.readFileSync(path, 'utf-8');
+    var lines = buff.split(/\r?\n/);
+    return lines;
+}
+function isDir(path) {
+    try {
+        var stat = fs.lstatSync(path);
+        return stat.isDirectory();
+    }
+    catch (ex) {
+        return false;
+    }
+}
---
+++ tools/build-index.ts
@@ -0,0 +1,140 @@
+import { Extractor } from '@angular/compiler';
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+main();
+
+type Article = {
+  id: string;
+  title: string;
+  path: string;
+  file: string;
+  tags: string[];
+};
+
+function main() {
+  console.log('BEGIN');
+
+  const ROOT = '../src/articles';
+
+  const articles: Article[] = [];
+
+  indexArticles(articles, ROOT);
+
+  writeIndex(articles, `${ROOT}/_index.json`);
+
+  console.log('Articles:', articles);
+
+  console.log('END');
+}
+
+function writeIndex(articles: Article[], path: string) {
+  if (articles.length == 0) {
+    throw new Error('Not articles to write.');
+  }
+
+  const json = {
+    updated: new Date().toISOString(),
+    articles,
+  };
+
+  const data = JSON.stringify(json, null, 2);
+
+  fs.writeFile(path, data, (err) => {
+    if (err) throw err;
+  });
+}
+
+function indexArticles(
+  articles: Article[],
+  root: string,
+  path?: string,
+  file?: string
+) {
+  const newPath = join([path, file], '/');
+  const fullPath = join([root, path, file], '/');
+
+  console.log('newPath', newPath);
+
+  if (isDir(fullPath)) {
+    const files: string[] = fs.readdirSync(fullPath);
+    files.forEach((file) => indexArticles(articles, root, newPath, file));
+  } else if (file?.match(/\.md$/)) {
+    const articlePath = path?.substring(1) || '';
+    const tags: string[] = [];
+    let title: string = '';
+    let values: string[] | undefined;
+
+    const NAME_REGEXP = /(.+)\.md$/g;
+    values = extractText(NAME_REGEXP, file);
+    const name = (values ? values[0] : file).toLowerCase();
+    const id = `${articlePath}/${name}`;
+
+    const lines = readFile(fullPath);
+    lines.forEach((line) => {
+      const TITLE_REGEXP = /^\s*#\s+(.+)/g;
+      if (!title) {
+        values = extractText(TITLE_REGEXP, line);
+        if (values) title = values[0];
+      }
+
+      const TAG_REGEXP = /#([a-z.]+)/g;
+      values = extractText(TAG_REGEXP, line);
+      if (values) {
+        pushTags(tags, values);
+      }
+
+      pushTags(tags, articlePath?.split('/') || []);
+    });
+
+    title = title || name;
+
+    const article: Article = {
+      id,
+      title,
+      path: articlePath,
+      file,
+      tags,
+    };
+
+    articles.push(article);
+  }
+}
+
+function join(vet: any[], sep: string): string {
+  return vet.reduce((acc, val) => (acc || '') + (val ? sep + val : ''));
+}
+
+function pushTags(tags: string[], values: string[]) {
+  values.forEach((value) => {
+    if (!tags.includes(value)) {
+      tags.push(value);
+    }
+  });
+}
+
+function extractText(regexp: RegExp, str: string): string[] | undefined {
+  const values: string[] = [];
+  let group;
+  while ((group = regexp.exec(str))) {
+    values.push(group[1]);
+  }
+
+  return values.length > 0 ? values : undefined;
+}
+
+function readFile(path: string): string[] {
+  const buff = fs.readFileSync(path, 'utf-8');
+  const lines = buff.split(/\r?\n/);
+
+  return lines;
+}
+
+function isDir(path: string) {
+  try {
+    const stat = fs.lstatSync(path);
+    return stat.isDirectory();
+  } catch (ex) {
+    return false;
+  }
+}
